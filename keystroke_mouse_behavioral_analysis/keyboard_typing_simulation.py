b4cc8228e14df2336731ebdd7fb39051db847bb6
hanhan <hanhanwu@users.noreply.github.com>
2019-11-21 21:44:06 -0800

    press_star works on mac

--- keystroke_mouse_behavioral_analysis/keyboard_typing_simulation.py
+++ keystroke_mouse_behavioral_analysis/keyboard_typing_simulation.py
@@ -3,7 +3,7 @@ import pyautogui
 # with typewrite(), it will type the string automatically
 pyautogui.typewrite("""The fox told the little prince, 'The most precious things are never visible.'""", interval=0.01)  # interval 0.01 sec
 
-# shift here only works on Windows, not on Mac
+# The code below only works on Windows, not on Mac
 ## To see this work, you need terminal, IPython won't show the result
 def press_star():
     pyautogui.keyDown('shift')
@@ -11,4 +11,6 @@ def press_star():
     pyautogui.keyUp('8')
     pyautogui.keyUp('shift')
     
-# TO BE CONTINUED...
+# To make Shift + 8 works on Mac
+def press_star():
+    pyautogui.hotkey("shift", "8")
